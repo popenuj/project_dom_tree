8b72b672df665ee94ed308ea2545475c4c04e5b1
Jessica G <gillan.jessica@gmail.com>
2016-11-10 16:37:55 -0700

    start tests

---
+++ spec/warmups_spec.rb
@@ -0,0 +1,26 @@
+require 'parser'
+
+describe Parser do
+  let(:header) { }
+  let(:div) { "<div id = 'bim'>" }
+  let(:p) { "<p class='foo bar' id='baz' name='fozzie'>" }
+  let(:img) { "<img src='http://www.example.com' title='funny things'>" }
+
+
+
+  describe "#parse_tag" do
+    it "returns a Tag struct" do
+    end
+
+    it "can parse header tags"
+
+    it "can parse p tags"
+
+    it "can parse img tags"
+
+    it "can parse div tags"
+
+
+  end
+
+end
