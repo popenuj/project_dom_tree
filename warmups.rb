61cebd2b36b73435c11e9806efa977ea056ed603
luke-schleicher <luke.j.schleicher@gmail.com>
2016-11-10 16:27:47 -0600

    working on html parser

--- warmups.rb
+++ warmups.rb
@@ -12,34 +12,65 @@ end
 
 Tag = Struct.new(:type, :attributes)
 
-def parse_tag(tag)
+class Parser
 
-  # take tag
-  # populate struct with information from the tag
+  def initialize(tag)
+    @tag = tag
+  end
 
-  tag_contents = tag.match(/[^<>]+/)[0] # "h1 id='dolphin'"  ... class='cass fef dsflkjdf'
+   def parse_tag
 
-  type = tag_contents.match(/(\w+)/)[0] # "h1"
-  tag_contents.del_first!(/(\w+)/) # " id='dolphin'"
+    parse_tag_type
+    parse_attributes_and_values
 
-  attribute = tag_contents.match(/(\w+)/)[0] # "id"
-  tag_contents.del_first!(/(\w+)/) # " ='dolphin'"
+    # take tag
+    # populate struct with information from the tag
 
-  attribute_val = tag_contents.match(/(\w+)/)[0] # "dolphin"
-  p attribute_val
-  tag_contents.del_first!(/(\w+)/) # " =''"
-  p tag_contents
+    # tag_contents = tag.match(/[^<>]+/)[0] # "h1 id='dolphin'"  ... class='cass fef dsflkjdf'
 
+    type = tag_contents.match(/(\w+)/)[0] # "h1"
+    tag_contents.del_first!(/(\w+)/) # " id='dolphin'"
 
+    attribute = tag_contents.match(/(\w+)/)[0] # "id"
+    tag_contents.del_first!(/(\w+)/) # " ='dolphin'"
 
+ # "dolphin"
+    # p attribute_val
+    tag_contents.del_first!(/(\w+)/) # " =''"
+    # p tag_contents
 
-  # test = Tag.new(type, nil)
-  # return test
+    # test = Tag.new(type, nil)
+    # return test
+  end
+
+  def parse_tag_type
+    tag_type = @tag.match(/(\w+)/)[0]
+    p tag_type
+  end
+
+  def parse_attributes_and_values
+    # grab each attribute/value pair and push to a hash
+
+    # <h1 id='dolphin bluewhale shark'>
+
+    # attribute_val = @tag.match(/\=\s*['|"](.*)['|"]/)
+
+    attribute_val = @tag.scan(/\s(.*)\=\s*['|"](.*)['|"]/)
+
+    p attribute_val
+  end
+
+  def parse_values
+  end 
+
 end
 
+test_tag = "<h1 id='dolphin bluewhale shark' class='animal amphibian' draggable='false'>"
+p = Parser.new(test_tag)
+p.parse_tag
 
 
-test_tag = parse_tag("<h1 id='dolphin'>")
+
 # puts test_tag
 
 # <tagtype attribute='attr_value' >
